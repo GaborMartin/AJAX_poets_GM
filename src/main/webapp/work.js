edde737036147897b1559dcfdf3384b64a0ae4bf
GaborMartin <gabormartin94@gmail.com>
2018-05-01 15:45:57 +0200

    Implemented onSearchClicked, onSearchResponse, searchSubstring

--- src/main/webapp/work.js
+++ src/main/webapp/work.js
@@ -1,3 +1,33 @@
+function searchSubstring(occurence) {
+    const occurenceEl = document.getElementById('occurence');
+    occurenceEl.textContent = occurence;
+}
+
+function onSearchResponse() {
+    clearMessages();
+    if (this.status === OK) {
+        searchSubstring(JSON.parse(this.responseText));
+    } else {
+        onOtherResponse(workContentDivEl, this);
+    }
+}
+
+function onSearchClicked() {
+    const seekerForm = document.forms['seeker'];
+
+    const subString = seekerForm.querySelector('input[name="substring"]');
+    const subStringValue = subString.value;
+
+    const params = new URLSearchParams();
+    params.append('substring', subStringValue);
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', onSearchResponse);
+    xhr.addEventListener('error', onNetworkError);
+    xhr.open('POST', 'protected/work');
+    xhr.send(params);
+    }
+
 function onWorkLoad(work) {
     const workContentSpanEl = document.getElementById('contentofwork');
     workContentSpanEl.textContent = work.content;
